d6a00b2d334f2e4106045ea35311f4e2417f28bf
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-08-02 23:24:51 +0100

    flaot

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-print("Hello World")
+# print("Hello World")
 #
 # Variables
 # use to temporarily store data in a computer's memory'
-age = 20
-price = 19.25
-first_name = 'Miskiat'
-is_online = False
-print(age)
+# age = 20
+# price = 19.25
+# first_name = 'Miskiat'
+# is_online = False
+# print(age)
 
 # input(use to read value from the terminal)
-name = input ("What is your name? ")
-print("Hello " + name)
+# name = input ("What is your name? ")
+# print("Hello " + name)
 
 # Type Conversion
 birth_year = input ("Enter your birth year: ")
-age = 2020 - birth_year
+age = 2023 - int(birth_year)
 print(age)
+
+# float(number with a decimal point)
+
+# float() for converting to a decimal
+# int() for converting to an integer
+# bool() for converting to a boolean
+# str() or converting to a string
+
+ first = input("First" )
+ second = input("Second" )
