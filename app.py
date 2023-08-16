bd4e072da99d8f9b550a796543813c3f151a3d89
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-08-16 22:17:42 +0100

    lists method

--- app.py
+++ app.py
@@ -140,7 +140,15 @@ print(names[0:3 ])
 print(names)
 
 # List Methods
-"a".
+numbers =[1, 2, 3, 4, 5]
+numbers.append(6)
+print(numbers)
+
+numbers.insert(0,-1)
+print(numbers)
+
+numbers.remove(3)
+print(numbers)
 
 
 
